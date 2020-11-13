ab00617d0049565c47b9eb734516d8b1cfcb12cd
Justine Licuanan <caissoone@gmail.com>
2020-11-13 11:57:38 +0800

    Design the recipe list component

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -4,16 +4,34 @@ import PropTypes from 'prop-types';
 const RecipeItem = ({ recipe }) => {
 	return (
 		<li className='card'>
-			<div>
+			<a
+				href={recipe.url.split(':').slice(1).join()}
+				target='_blank'
+				rel='noopener noreferrer'
+			>
 				<img src={recipe.image} alt={recipe.label} />
-			</div>
+			</a>
 			<div>
-				<h1>{recipe.label}</h1>
+				<a
+					href={recipe.url.split(':').slice(1).join()}
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<h1>{recipe.label}</h1>
+				</a>
 				<ul>
 					{recipe.ingredientLines.map((ingredient, index) => (
-						<li key={index}>{ingredient}</li>
+						<li key={index}>{`• ${ingredient}`}</li>
 					))}
 				</ul>
+				<a
+					href={recipe.url.split(':').slice(1).join()}
+					target='_blank'
+					rel='noopener noreferrer'
+					className='btn'
+				>
+					Instructions
+				</a>
 			</div>
 		</li>
 	);
